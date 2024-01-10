73d67edc26e4607de2020774b762a0a932190991
Guillaume Denis <gdenispro@gmail.com>
2024-01-10 13:22:12 +0100

    update deps and tests to be compatibles with wsmock 0.2

--- go.mod
+++ go.mod
@@ -1,9 +1,11 @@
 module github.com/ducksouplab/ducksoup
 
-go 1.20
+go 1.21
+
+toolchain go1.21.5
 
 require (
-	github.com/evanw/esbuild v0.19.10
+	github.com/evanw/esbuild v0.19.11
 	github.com/google/uuid v1.5.0
 	github.com/gorilla/mux v1.8.1
 	github.com/gorilla/websocket v1.5.1
@@ -15,7 +17,7 @@ require (
 	github.com/pion/turn/v2 v2.1.4
 	github.com/pion/webrtc/v3 v3.2.24
 	github.com/rs/zerolog v1.31.0
-	github.com/silently/wsmock v0.1.6
+	github.com/silently/wsmock v0.2.0
 	gonum.org/v1/plot v0.14.0
 	gopkg.in/yaml.v2 v2.4.0
 )
@@ -34,7 +36,7 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pion/datachannel v1.5.5 // indirect
-	github.com/pion/dtls/v2 v2.2.8 // indirect
+	github.com/pion/dtls/v2 v2.2.9 // indirect
 	github.com/pion/logging v0.2.2 // indirect
 	github.com/pion/mdns v0.0.9 // indirect
 	github.com/pion/randutil v0.1.0 // indirect
@@ -45,10 +47,10 @@ require (
 	github.com/pion/transport/v2 v2.2.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
-	golang.org/x/crypto v0.17.0 // indirect
-	golang.org/x/image v0.14.0 // indirect
-	golang.org/x/net v0.19.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/image v0.15.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- sfu/peer_server_test.go
+++ sfu/peer_server_test.go
@@ -9,6 +9,8 @@ import (
 	"github.com/silently/wsmock"
 )
 
+var durationUnit = 100 * time.Millisecond
+
 func messageInWithPayload(kind string, payload any) messageIn {
 	m, _ := json.Marshal(payload)
 	return messageIn{kind, string(m)}
@@ -19,24 +21,24 @@ func TestRunPeerServer_Join(t *testing.T) {
 		conn, rec := wsmock.NewGorillaMockAndRecorder(t)
 		go RunPeerServer("http://origin.test", conn)
 		conn.Send(messageIn{"unknown", ""})
-		rec.AssertFirstReceived(messageOut{Kind: "error-join"})
-		rec.Run(200 * time.Millisecond)
+		rec.NewAssertion().NextToBe(messageOut{Kind: "error-join"})
+		rec.RunAssertions(durationUnit)
 	})
 
 	t.Run("fails when join message does not contain an interactionName", func(t *testing.T) {
 		conn, rec := wsmock.NewGorillaMockAndRecorder(t)
 		go RunPeerServer("http://origin.test", conn)
 		conn.Send(messageInWithPayload("join", types.JoinPayload{UserId: "user1"}))
-		rec.AssertFirstReceived(messageOut{Kind: "error-join"})
-		rec.Run(200 * time.Millisecond)
+		rec.NewAssertion().NextToBe(messageOut{Kind: "error-join"})
+		rec.RunAssertions(durationUnit)
 	})
 
 	t.Run("fails when join message does not contain a userId", func(t *testing.T) {
 		conn, rec := wsmock.NewGorillaMockAndRecorder(t)
 		go RunPeerServer("http://origin.test", conn)
 		conn.Send(messageInWithPayload("join", types.JoinPayload{InteractionName: "interaction1"}))
-		rec.AssertFirstReceived(messageOut{Kind: "error-join"})
-		rec.Run(200 * time.Millisecond)
+		rec.NewAssertion().NextToBe(messageOut{Kind: "error-join"})
+		rec.RunAssertions(durationUnit)
 	})
 
 	t.Run("succeeds when join message is complete", func(t *testing.T) {
@@ -46,8 +48,8 @@ func TestRunPeerServer_Join(t *testing.T) {
 			InteractionName: "interaction1",
 			UserId:          "user1",
 		}))
-		rec.AssertReceivedContains("joined")
-		rec.Run(200 * time.Millisecond)
+		rec.NewAssertion().OneToContain("joined")
+		rec.RunAssertions(durationUnit)
 	})
 }
 
@@ -59,7 +61,7 @@ func TestRunPeerServer_Offer(t *testing.T) {
 			InteractionName: "interaction2",
 			UserId:          "user2",
 		}))
-		rec.AssertReceivedContains("offer")
-		rec.Run(1000 * time.Millisecond)
+		rec.NewAssertion().OneToContain("offer")
+		rec.RunAssertions(10 * durationUnit)
 	})
 }
